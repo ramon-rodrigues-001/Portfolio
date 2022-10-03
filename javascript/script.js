8d05d19cfd25639bc6036b0b68b31b7edc34d06a
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-03 06:59:27 -0300

    Mostrar menos habilidade

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,26 @@
+// PARA MUDAR O TEXTO DO BUTÃO MOSTRAR MAIS DA PARTE [PROJETOS]
 let input_projetos = document.querySelector('input#mais-projetos')
-let a = document.querySelector('a#btn-mais-projetos')
+let a_projetos = document.querySelector('a#btn-mais-projetos')
 
-function mostrar() {
+function mostrar_projetos() {
     if (input_projetos.checked == false) {
-        a.innerHTML = "MOSTRAR MENOS"
+        a_projetos.innerHTML = "MOSTRAR MENOS"
     }
     else if (input_projetos.checked == true) {
-        a.innerHTML = "MOSTRAR MAIS"
+        a_projetos.innerHTML = "MOSTRAR MAIS"
+    }
+}
+
+
+// PARA MUDAR O TEXTO DO BUTÃO MOSTRAR MAIS DA PARTE [HABILIDADES]
+let input_habilidades = document.querySelector('input#mais-habilidades')
+let a_habilidades = document.querySelector('a#btn-mais-habilidades')
+
+function mostrar_habilidades() {
+    if (input_habilidades.checked == false) {
+        a_habilidades.innerHTML = "MOSTRAR MENOS"
+    }
+    else if (input_habilidades.checked == true) {
+        a_habilidades.innerHTML = "MOSTRAR MAIS"
     }
 }
