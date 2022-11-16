4f609dfcb721f38f06d6025c09a573f35e743890
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-11-16 18:49:50 -0300

    mais

--- javascript/text-habilidades.js
+++ javascript/text-habilidades.js
@@ -4,8 +4,10 @@ export const clicarEmCards = function (ev) {
 
     const h1Inicial = document.querySelector('#titliInicial')
     document.querySelector('#conteiner-resposta-abilidade').removeChild(h1Inicial)
-
-    const elemento = ev.currentTarget
+    const paragrafInicial = document.querySelector('#paragrafInicial')
+    document.querySelector('#conteiner-resposta-abilidade').removeChild(paragrafInicial)
+    
+    let elemento = ev.currentTarget
 
     switch (elemento.id) {
         case 'h1':
@@ -41,31 +43,31 @@ export const clicarEmCards = function (ev) {
 const text = document.querySelector('#text-resposta')
 
 function html() {
-    criar('HTML', 'Eu estudei por muito tempo html')
+    criar('HTML5', 'O esqueleto do projeto blablabla Facil para dedeu')
 }
 function css() {
-    alert('css')
+    criar('CSS', 'Esse vai dar uma boa dor de cabeça kkkk, Brincadeira CSS é a melhor parte.')
 }
 function javascript() {
-    alert('javascript')
+    criar('JavaScript', 'Estou nesmo momento estudando javascript, afinal não tem fim, estou a todo momento estarei estudando essa linguagem incrivel')
 }
 function sass() {
-    alert('sass')
+    criar('Sass', 'Que feramenta massa, estilizar fica muito mais intuitiva com ela')
 }
 function bootstrap() {
-    alert('bootstrap')
+    criar('Bootstrap', 'Ao')
 }
 function python() {
 criar('Python', 'AAah Python, a primeira linguagem de programação a qual tive contato passei muito tempo nesta linguagem aprendendo basicamente logica, as vezes volto e dou uma praticadinha, mais não é mais meu foco')
 }
 function git() {
-    alert('git')
+    criar('Git')
 }
 function typescript() {
-    alert('typescript')
+    criar('TypeScript')
 }
 function bulma() {
-    alert('bulma')
+    criar('bulma')
 }
 
 
@@ -74,9 +76,13 @@ function criar(title, text) {
 
     const h1 = document.createElement('h2')
     h1.innerText = title
+    h1.id = 'titliInicial'
+    h1.dataset.aos = 'flip-right'
 
     const p = document. createElement('p')
     p.innerText = text
+    p.id = 'paragrafInicial'
+    p.dataset.aos = 'flip-right'
 
     containerDeTexto.append(h1, p)
 }
