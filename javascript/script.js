992d0526de47718edee591f1e50ef752496a26b8
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-03-26 19:00:48 -0300

    texto sobre mim completo

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,52 @@
-// PARA MUDAR O TEXTO DO BUTÃO MOSTRAR MAIS DA PARTE [PROJETOS]
-let input_projetos = document.querySelector('input#mais-projetos')
-let a_projetos = document.querySelector('a#btn-mais-projetos')
 
-function mostrar_projetos() {
+// PARA MUDAR O TEXTO DO BUTÃO MOSTRAR MAIS DA PARTE [Sobre Mim]
+let a_sobreMim = document.querySelector('a#btn-mais-sobreMim')
+a_sobreMim.addEventListener('click', ()=> {
+    let input_sobreMim = document.querySelector('#mais-sobreMim')
+
+    if (input_sobreMim.checked == false) {
+        a_sobreMim.innerText = "MOSTRAR MENOS"
+    }
+    else if (input_sobreMim.checked == true) {
+        a_sobreMim.innerText = "MOSTRAR MAIS"
+    }
+})
+
+
+
+
+// PARA MUDAR O TEXTO DO BUTÃO MOSTRAR MAIS DA PARTE [PROJETOS]
+let a_projetos = document.querySelector('a#btn-mais-projetos')
+a_projetos.addEventListener('click', ()=> {
+    let input_projetos = document.querySelector('input#mais-projetos')
+
     if (input_projetos.checked == false) {
         a_projetos.innerHTML = "MOSTRAR MENOS"
     }
     else if (input_projetos.checked == true) {
         a_projetos.innerHTML = "MOSTRAR MAIS"
     }
-}
+})
+
+
+
 
 
 // PARA MUDAR O TEXTO DO BUTÃO MOSTRAR MAIS DA PARTE [HABILIDADES]
-let input_habilidades = document.querySelector('input#mais-habilidades')
 let a_habilidades = document.querySelector('a#btn-mais-habilidades')
+a_habilidades.addEventListener('click', ()=> {
+    let input_habilidades = document.querySelector('input#mais-habilidades')
 
-function mostrar_habilidades() {
     if (input_habilidades.checked == false) {
         a_habilidades.innerText = "MOSTRAR MENOS"
     }
     else if (input_habilidades.checked == true) {
         a_habilidades.innerText = "MOSTRAR MAIS"
     }
-}
+})
+
+
+
 
 
 // TEXT OF HABILITY
